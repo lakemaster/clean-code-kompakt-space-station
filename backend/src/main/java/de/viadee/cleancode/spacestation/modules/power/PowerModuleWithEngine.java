c193879c9435999ba58ba3d71857d8aa890373fa
lakemaster <lakelord@jojobi.de>
2021-02-25 08:01:10 +0100

    Refactor

--- backend/src/main/java/de/viadee/cleancode/spacestation/modules/power/PowerModuleWithEngine.java
+++ backend/src/main/java/de/viadee/cleancode/spacestation/modules/power/PowerModuleWithEngine.java
@@ -1,73 +1,87 @@
 package de.viadee.cleancode.spacestation.modules.power;
 
-import de.viadee.cleancode.spacestation.modules.DailyWorkResult;
 import org.springframework.stereotype.Component;
 
+import de.viadee.cleancode.spacestation.modules.DailyWorkResult;
+
 @Component
-public class PowerModuleWithEngine extends PowerModule {
+public class PowerModuleWithEngine {
+
+	private PowerModule powerModule;
 
 	private boolean engineActivated = false;
 
 	public DailyWorkResult doDailyWork() {
-		DailyWorkResult result = super.doDailyWork();
+		DailyWorkResult result = this.powerModule.doDailyWork();
 		if (result.getReturnCode().equals("0")) {
-			startEngine();
-			// ...
-			stopEnginge();
+			this.doDailyEngineWork();
 		}
 		return result;
 	}
 
-	private void startEngine() {
-		if (engineActivated && powerPackCapacity >= 1) {
-			engineActivated = true;
-			powerPackCapacity--;
-		}
-		return;
+	private void doDailyEngineWork() {
+		this.startEngine();
+		// ...
+		this.stopEngine();
 	}
 
-	private void stopEnginge() {
-		engineActivated = false;
+	private void startEngine() {
+		if (!this.engineActivated && this.powerModule.powerPackCapacity >= 1) {
+			this.engineActivated = true;
+			this.powerModule.powerPackCapacity--;
+		}
+	}
+
+	private void stopEngine() {
+		this.engineActivated = false;
 	}
 
 	public DailyWorkResult navigate(Destination destination) {
-		int powerConsumptionBase = 40;
-		int powerConsumption = 0;
-		switch (destination) {
-		case RED_PLANET:
-			powerConsumption = powerConsumptionBase << 1;
-			break;
-		case ORANGE_PLANET:
-			powerConsumption = powerConsumptionBase << 1;
-			break;
-		case GREEN_PLANET:
-			powerConsumption = powerConsumptionBase << 2;
-			break;
-		case YELLOW_PLANET:
-			powerConsumption = powerConsumptionBase >> 1;
-			break;
-		case BLUE_PLANET:
-			powerConsumption = powerConsumptionBase << 3;
-			break;
-		case WHITE_PLANET:
-			powerConsumption = powerConsumptionBase >> 2;
-			break;
-		case MOON_DARK_SIDE:
-			powerConsumption = powerConsumptionBase % powerConsumptionBase;
-			break;
-		case STATIONARY_ORBIT:
-		default:
-			powerConsumption = powerConsumptionBase;
-		}
+		int powerConsumption = this.calculatePowerConsumption(destination);
+		return this.fly(destination, powerConsumption);
+	}
+
+	// Kein schöner Name
+	private DailyWorkResult fly(Destination destination, int powerConsumption) {
 		DailyWorkResult result = new DailyWorkResult();
-		if (powerPackCapacity % powerConsumption > 0) {
+		if (this.powerModule.powerPackCapacity < powerConsumption) {
 			result.setReturnCode("-1");
 			result.setErrorMessage("Destination cannot be reached - recharge power pack first");
 		} else {
-			powerPackCapacity = powerPackCapacity - powerConsumption;
+			this.powerModule.powerPackCapacity = this.powerModule.powerPackCapacity - powerConsumption;
 			result.setReturnCode("0");
 			result.setErrorMessage("Destination " + destination.toString() + " successfully reached.");
 		}
 		return result;
+	}
+
+	private int calculatePowerConsumption(Destination destination) {
+		int powerConsumptionBase = 40;
+		int powerConsumption = 0;
+		switch (destination) {
+			case RED_PLANET:
+			case ORANGE_PLANET:
+				powerConsumption = powerConsumptionBase * 2;
+				break;
+			case GREEN_PLANET:
+				powerConsumption = powerConsumptionBase * 4;
+				break;
+			case YELLOW_PLANET:
+				powerConsumption = powerConsumptionBase / 2;
+				break;
+			case BLUE_PLANET:
+				powerConsumption = powerConsumptionBase * 8;
+				break;
+			case WHITE_PLANET:
+				powerConsumption = powerConsumptionBase / 4;
+				break;
+			case MOON_DARK_SIDE:
+				powerConsumption = 0;
+				break;
+			case STATIONARY_ORBIT:
+			default:
+				powerConsumption = powerConsumptionBase;
+		}
+		return powerConsumption;
 	}
 }
